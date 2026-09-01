de1d34de22e4b25ddb02391f71b6087b622b4775
Dinkar Choudhary <gravatoreditz@gmail.com>
2026-09-01 23:53:58 +0530

    Create a simple HTTP server in createserver.js

---
+++ createserver.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const server = http.createServer((req, res) => (
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/plain');
+res.end('Hello world');
+));
+server.listen(3000, ()=>(
+console.log('Server chal raha hai port 3000 par');
