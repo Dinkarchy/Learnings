b22fcc2b18bbec4053c742a190a10336a1c8b421
Dinkar Choudhary <gravatoreditz@gmail.com>
2026-09-02 15:00:49 +0530

    Add files via upload

--- createserver.js
+++ createserver.js
@@ -1,8 +1,57 @@
 const http = require('http');
-const server = http.createServer((req, res) => (
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hello world');
-));
-server.listen(3000, ()=>(
-console.log('Server chal raha hai port 3000 par');
+const {url} = require('url');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const query = parsedUrl.query;
+
+    console.log(`request recieved: ${req.method} ${path}`);
+
+    if(path == '/' && req.method === 'GET'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Welcome! Try /greet?name=Rahul or /headers or POSTto /data\n');
+    }
+
+    else if(path == '/greet' && req.method === 'GET'){
+        const name =query.name || 'Guest';
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`Hello, ${name}! Welcome to the server.\n`);
+    }
+
+    else if(path === '/headers' && req.method === 'GET'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(req.headers, null, 2));
+    }
+
+    else if( path === '/data' && req.method === 'POST'){
+        let body = '';
+        req.on('data', chunk => { body += chunk;})
+        req.on('end', () => {
+            res.statusCode = 201;
+            res.setHeader('Content-type', 'application/json');
+            res.end(JSON.stringify({message: 'Data recieved seccesfully', yourData: body}));
+        });
+    }
+
+    else if(path === '/error'){
+        res.statusCode= 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('stimulated server error (500)\n');
+    }
+    else{
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('404 - Page Not Found')
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
