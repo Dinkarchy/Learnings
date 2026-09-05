b048dac980b3d9ac46e5959a351beb4f5e38375f
Dinkar Choudhary <gravatoreditz@gmail.com>
2026-09-05 23:52:55 +0530

    Add files via upload

---
+++ exp3_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class student{
+    string stName;
+    long rollNo;
+    int marks;
+    static int sudentCount;
+};
+
+int main() {
+    
+    return 0;
+}
