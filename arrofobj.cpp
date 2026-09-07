8faff1ff4fc8348c2b6efb88081ac6b9f1a252c5
Dinkar Choudhary <gravatoreditz@gmail.com>
2026-09-07 23:36:54 +0530

    Add files via upload

---
+++ arrofobj.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class AB {
+    int i;
+public: 
+    AB(int j) {
+        i = j;
+    }
+
+    int get_i() {
+        return i;
+    }
+};
+
+int main() {
+    AB obj[3] = {1, 2, 3};
+    AB *p = obj;
+
+    for(int i = 0; i < 3; i++) {
+        cout << obj[i].get_i() << " ";
+    }
+    cout << endl;
+
+    for(int i = 0; i < 3; i++) {
+        cout << (p+i)->get_i() << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
