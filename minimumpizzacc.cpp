846b2b118335d8dabc019fa80e58dd93f5f897d6
Dinkar Choudhary <gravatoreditz@gmail.com>
2026-09-12 23:58:13 +0530

    Implement minimum pizza calculation program

---
+++ minimumpizzacc.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+
+        cout << (N * X + 3) / 4 << '\n';
+    }
+
+    return 0;
+}
